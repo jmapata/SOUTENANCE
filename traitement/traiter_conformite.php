6acd44b8bf45810afc0436690bd61c977aaf50a4
jmapata <apatajeanmichel2005@gmail.com>
2025-07-19 14:04:26 +0200

    Notification email rapport modification

--- traitement/traiter_conformite.php
+++ traitement/traiter_conformite.php
@@ -2,6 +2,7 @@
 // traitement/traiter_conformite.php (Version finale avec UPSERT)
 session_start();
 require_once '../config/database.php';
+require_once '../config/mailer.php';
 
 function send_json_response(array $data) {
     if (ob_get_level()) ob_end_clean();
@@ -64,6 +65,21 @@ try {
     
     $stmt_log = $pdo->prepare($sql_log);
     $stmt_log->execute([$personnel_id, $rapport_id, $statut_conformite_log, $commentaire]);
+
+    // Envoi d'un email à l'étudiant si le rapport est non conforme
+    if ($decision === 'non_conforme') {
+        // Récupérer l'email et le nom de l'étudiant concerné
+        $stmt_etud = $pdo->prepare("SELECT u.email_principal, e.prenom, e.numero_carte_etudiant FROM rapport_etudiant r JOIN etudiant e ON r.numero_carte_etudiant = e.numero_carte_etudiant JOIN utilisateur u ON e.numero_utilisateur = u.numero_utilisateur WHERE r.id_rapport_etudiant = ? LIMIT 1");
+        $stmt_etud->execute([$rapport_id]);
+        $etud = $stmt_etud->fetch();
+        if ($etud && !empty($etud['email_principal'])) {
+            $dest_email = $etud['email_principal'];
+            $dest_nom = trim(($etud['prenom'] ?? '') . ' ' . ($etud['numero_carte_etudiant'] ?? ''));
+            $sujet = 'Votre rapport n\'est pas conforme - ValidMaster';
+            $contenu = "<p>Bonjour <b>" . htmlspecialchars($dest_nom) . ",<br>Votre rapport a été jugé <b>non conforme</b> par le contrôle de conformité.<br><br><i>" . "</i><br><br>Veuillez vous connecter à la plateforme ValidMaster pour corriger ou soumettre un nouveau rapport.<br><br>Cordialement,<br>L'équipe ValidMaster.</p>";
+            envoyerEmail($dest_email, $dest_nom, $sujet, $contenu);
+        }
+    }
     // ## AUDIT DE L'ACTION ##
     $id_action_audit = ($decision === 'conforme') ? 'CONFORMITE_RAPPORT_APPROUVE' : 'CONFORMITE_RAPPORT_REJETE';
     $audit_id = 'AUDIT-' . strtoupper(uniqid());
